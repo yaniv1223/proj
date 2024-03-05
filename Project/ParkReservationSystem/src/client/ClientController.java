5113eaf2b5e395563aa86b0abca507406945d736
AdarCohen1 <Adarcohen1@gmail.com>
2024-03-05 14:27:02 +0200

    123

--- Project/ParkReservationSystem/src/client/ClientController.java
+++ Project/ParkReservationSystem/src/client/ClientController.java
@@ -18,7 +18,7 @@ import common.ChatIF;
  * @version July 2000
  */
 public class ClientController implements ChatIF 
-{//this is test1
+{//this is test
   //Class variables *************************************************
   
   /**
